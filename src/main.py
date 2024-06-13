3d1ce19e78a5e452087278a76b98827953c9369e
Эдуард <eduard@MacBook-Air-Eduard.local>
2024-06-14 01:55:24 +0500

    Architecture

---
+++ src/main.py
@@ -0,0 +1,43 @@
+from fastapi import Depends, FastAPI
+from fastapi_users import FastAPIUsers, fastapi_users
+
+from auth.auth import auth_backend
+from auth.manager import get_user_manager
+from auth.models import User
+from auth.shemas import UserCreate, UserRead
+
+from operations.router import router as router_operation
+
+app = FastAPI(title="Trading app")
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(router_operation)
+
+
+current_user = fastapi_users.current_user()
+
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
+
+
+@app.get("/unprotected-route")
+def protected_route():
+    return f"Hello, anonym!"
