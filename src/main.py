3040891fb51a18aed5b601fb4f47b1893930a424
Eduard <rusgaymtuan@gmail.com>
2024-06-15 03:27:28 +0500

    Set up redis

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+import uvicorn
+
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+import time
+
+from redis import asyncio as aioredis
+from redis.asyncio.connection import ConnectionPool
+
 from fastapi import Depends, FastAPI
 from fastapi_users import FastAPIUsers, fastapi_users
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
 
 from auth.auth import auth_backend
 from auth.manager import get_user_manager
@@ -8,7 +20,22 @@ from auth.shemas import UserCreate, UserRead
 
 from operations.router import router as router_operation
 
-app = FastAPI(title="Trading app")
+
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    redis = aioredis.from_url("redis://localhost")
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    yield
+
+
+app = FastAPI(title="Trading app", lifespan=lifespan)
+
+
+@app.get("/long_operation")
+@cache(expire=60)
+def get_long_op():
+    time.sleep(2)
+    return "Куча данных"
+
 
 fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
@@ -41,3 +68,7 @@ def protected_route(user: User = Depends(current_user)):
 @app.get("/unprotected-route")
 def protected_route():
     return f"Hello, anonym!"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
