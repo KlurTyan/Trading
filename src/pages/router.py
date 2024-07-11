8adcb470034c88aa929d659f912e407e296efea0
Eduard <rusgaymtuan@gmail.com>
2024-07-11 18:49:29 +0500

    Create chat

--- src/pages/router.py
+++ src/pages/router.py
@@ -21,3 +21,8 @@ def get_base_search(request: Request, operations=Depends(get_specific_operations
     return templates.TemplateResponse(
         "search.html", {"request": request, "operations": operations["data"]}
     )
+
+
+@router.get("/chat")
+def get_chat_page(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
