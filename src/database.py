aeeb0bf35e5cc07d20cabc580b1ba0be4436ffef
Eduard <rusgaymtuan@gmail.com>
2024-07-11 18:49:05 +0500

    Small adjustments

--- src/database.py
+++ src/database.py
@@ -1,12 +1,14 @@
-import datetime
 from typing import AsyncGenerator
+from sqlalchemy import MetaData
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+from sqlalchemy.orm import DeclarativeBase
 
 from .config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(
@@ -17,3 +19,10 @@ async_session_maker = async_sessionmaker(
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+metadata = MetaData()
